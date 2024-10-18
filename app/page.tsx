2533fd535763f5ff2590fc4bb36c7a9a0c6502f5
william <peoch.william44@gmail.com>
2024-10-18 15:21:17 +0200

    playing with the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import Image from "next/image";
+import { db } from "@/drizzle/db";
+import { openingPosts, replies } from "@/drizzle/schema";
+import { sql } from "drizzle-orm";
+import Link from "next/link";
 
-export default function Home() {
+
+export default async function Home() {
+  const openingPostsRes = await db
+    .select({
+      id: openingPosts.id,
+      content: openingPosts.content,
+      replyCount: sql<number>`count(${replies.id})`,
+    })
+    .from(openingPosts)
+    .leftJoin(replies, sql`${openingPosts.id} = ${replies.openingPostId}`)
+    .groupBy(openingPosts.id)
+    .orderBy(sql`${openingPosts.lastReplyCreatedAt} desc`);
+
   return (
     <main>
-      test
+      <p>hello</p>
+      <div>
+        {openingPostsRes.map((openingPost) => {
+          return (
+            <Link key={openingPost.id} href={"/"}>
+            <p>{openingPost.content}</p>
+          </Link>
+          );
+        })}
+      </div>
     </main>
   );
 }
